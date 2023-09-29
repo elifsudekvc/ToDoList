b3a2f5cd5e09ea2e6568584ae5ef6d2f1596c78b
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2023-09-29 23:16:31 +0300

    localStorage, showAlert

--- app.js
+++ app.js
@@ -1,54 +1,89 @@
 //HTML etiketlerini seçmek
-
 const form = document.querySelector("#todoAddForm");
 const addInput = document.querySelector("#todoName");
 const todoList = document.querySelector(".list-group");
-const firstCardBody = document.querySelectorAll(".list-group")[0];
-const secondCardBody = document.querySelectorAll(".list-group")[1];
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
 const clearButton = document.querySelector("#clearButton");
+let todos=[];
+runEvents()
 
-runEvents();
+function runEvents(e) {
+ form.addEventListener("submit",addToDo);
+};
 
-function runEvents() {
-
-    form.addEventListener("submit",addToDo);
-
-}
-
-function addToDo(e){
-
+function addToDo(e) {
     const inputText = addInput.value.trim();
-    if(inputText==null || inputText==""){
-        alert("Lütfen bir değer giriniz...");
+    if(inputText==""){
+        showAlert("warning","Lütfen bir değer giriniz");
     }
     else{
-    //arayüze ekleme 
-    addToDoUI(inputText);
+    //arayüz 
+        addToDoUI(inputText);
+    //storage
+        addToDoStorage(inputText);
+        showAlert("success","ToDo eklendi.")
     }
-    
-    //storage ekleme
+    console.log("submit çalıştı");
+    e.preventDefault();
+};
 
-    console.log("Submit eventi çalıştı");
-    e.preventDefault(); //submitte farklı bir sayfaya yönlendirdiği için farklı sayfaya yönlendirmeyi engelledik.
 
+function addToDoUI(newToDo){
+    // <!--
+    //                     <li class="list-group-item d-flex justify-content-between">Todo 1
+    //                         <a href="#" class="delete-item">
+    //                             <i class="fa fa-remove"></i>
+    //                         </a>
+    //                     </li>
+    //                 -->
+
+    const li= document.createElement("li");
+    li.className="list-group-item d-flex justify-content-between"
+    li.textContent=newToDo
+
+    const a= document.createElement("a");
+    a.className="delete-item"
+    a.href="#"
+
+    const i=document.createElement("i");
+    i.className="fa fa-remove"
+
+    a.appendChild(i);
+    li.appendChild(a);
+    todoList.appendChild(li);
+
+    addInput.value="";
+
+};
+
+function addToDoStorage(newToDo) {
+    checkTodosFromStorage();
+    todos.push(newToDo);
+    localStorage.setItem("todos",JSON.stringify(todos));
+};
+
+function checkTodosFromStorage(){
+    if(todlocalStorage.getItem("todos")===null){
+        todos=[];
+    }else{
+        todos=JSON.parse(localStorage.getItem("todos"));
+    }
 }
 
-function addToDoUI(newTodo){
-    /*
-    <li class="list-group-item d-flex justify-content-between">Todo 1
-                            <a href="#" class="delete-item">
-                                <i class="fa fa-remove"></i>
-                            </a>
-                        </li>
-                        */
-const li = document.createElement("li");
-li.className="list-group-item d-flex justify-content-between"
-li.textContent=newTodo;
+function showAlert(type,message){
+//     <div class="alert alert-warning" role="alert">
+//   This is a warning alert—check it out!
+// </div>
 
-const a=document.createElement("a");
-a.href = "#"
-a.className="delete-item";
+const div=document.createElement("div");
+div.className="mt-2 alert alert-"+type;
+div.textContent=message;
+firstCardBody.appendChild(div);
 
-const i=document.creatElement(i);
-i.className="fa fa-remove";
+setTimeout(function(){
+div.remove();
+
+},2500);
+
 }
